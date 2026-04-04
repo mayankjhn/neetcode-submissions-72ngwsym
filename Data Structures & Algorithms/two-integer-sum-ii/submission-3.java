3e19be1630c33fbfb61d19b902b9d03e1ae60f4e
Mayank Agarwal <agrawalmayank2004@gmail.com>
2026-04-04 18:51:52 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int n = arr.length;
+        int[] ans = new int[2];
+        
+        for(int i = 0; i<n; i++){
+            boolean flag = false;
+            for(int j =i+1; j<n; j++){
+                if(arr[i]+arr[j] == target){
+                    ans[0]=i+1;
+                    ans[1]=j+1;
+                    flag=true;
+                }
+            }
+            if (flag==true){
+                break;
+            }
+        }
+        return ans;
+    }
+}
